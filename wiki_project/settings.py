e396bd04bede0c7b111f5f0a450de3548b548544
manvish-06 <manvishreddy06@gmail.com>
2026-08-12 18:20:05 +0530

    Improve production error logging

--- wiki_project/settings.py
+++ wiki_project/settings.py
@@ -181,3 +181,20 @@ if not DEBUG:
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
+
